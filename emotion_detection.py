696fda402756c28cb6d47795f8f50470b9964299
shwethavinay21 <shwethabaradwaj28@gmail.com>
2025-06-07 10:33:17 -0400

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,38 @@
+import requests  # Import the requests library to handle HTTP requests
+import json
+
+def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
+    # URL of the emotion analysis service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    # Custom header specifying the model ID for the emotion analysis service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyse } }   
+
+    # Send a POST request to the API with the text and headers  
+    response = requests.post(url, json = myobj, headers = header)
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting emotion including anger, disgust, fear, joy and sadness, along with their scores.
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    # Extracting the dominant emotion which is the emotion with highest score
+    dominant_emotion = max(formatted_response, key=scores.get)
+
+    # Returning a dictionary containing emotion detection results
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+            }
