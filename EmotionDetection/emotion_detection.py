c18ac809f95ed86ce18fefb815a8c193b9f94fa3
shwethavinay21 <shwethabaradwaj28@gmail.com>
2025-06-10 17:25:38 -0400

    server with error code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,8 @@
 import requests  # Import the requests library to handle HTTP requests
 import json
 
+"""
+"""
 def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
     # URL of the emotion analysis service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -17,16 +19,26 @@ def emotion_detector(text_to_analyse):  # Define a function named emotion_detect
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
-    # Extracting emotion including anger, disgust, fear, joy and sadness, along with their scores.
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        # Extracting emotion including anger, disgust, fear, joy and sadness, along with their scores.
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
-    # Extracting the dominant emotion which is the emotion with highest score
-    highest_score = {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,'joy': joy_score,'sadness': sadness_score}
-    dominant_emotion = max(highest_score, key=highest_score.get)
+        # Extracting the dominant emotion which is the emotion with highest score
+        highest_score = {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,'joy': joy_score,'sadness': sadness_score}
+        dominant_emotion = max(highest_score, key=highest_score.get)
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
     # Returning a dictionary containing emotion detection results
     return {
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
+"""
+"""
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
@@ -19,8 +21,12 @@ def sent_analyzer():
     sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    # Return a formatted string with the emotions and score
-    return "For the given statement, the system response is {}, {}, {}, {}, {}. The dominant emotion is {}." .format(anger_score,disgust_score,fear_score,joy_score,sadness_score,dominant_emotion)
+    # check if the score is none, indicating an error or invalid input
+    if dominant_emotion is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the emotions and score
+        return "For the given statement, the system response is {}, {}, {}, {}, {}. The dominant emotion is {}." .format(anger_score,disgust_score,fear_score,joy_score,sadness_score,dominant_emotion)
     
 # Render the HTML template using render_index_page
 @app.route("/")
